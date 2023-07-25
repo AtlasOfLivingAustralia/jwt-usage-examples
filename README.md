7603bd2d0ed2062a79d6b311622a38d685521726
Sushant <105408469+sughics@users.noreply.github.com>
2023-07-25 12:05:41 +1000

    adding further usage exmaple to readme, adding support for token refresh clients registered without clientSecret

--- README.md
+++ README.md
@@ -3,13 +3,17 @@ Example scripts showing JWT token usage and refresh based on a user provided JSO
 
 The script will attempt to make a request to a protected ALA API. If the token in the JSON file is not expired, it will send the token with the request. If the token has expired,
 
-1. New token will be generated against the tokenUrl using the `refresh_token` and `scope` from the JSON file along with  `clientId`, `clientSecret` supplied in the CLI.
+1. New token will be generated against the tokenUrl using the `refresh_token` and `scope` from the JSON file along with  `clientId`, `clientSecret` supplied in the CLI. If the Client App was registered without `clientSecret`, it is not required. 
+
 2. New token will be kept in memory and used for subsequent request and will again if updated upon expiry.
 
-The token refresh operation described can be successfully run regardless of whether the initial token was generated using just the simple Client Credentials or Client Credential AND User credentials e.g. Authorization Code Flow, Implicit Flow, Password Credentials etc. In both cases, no user credentials is required for token refresh  - only the `clientId`, `clientSecret` is required.
+The token refresh operation described can be successfully run regardless of whether the initial token was generated using just the simple Client Credentials or Client Credential AND User credentials e.g. Authorization Code Flow, Implicit Flow, Password Credentials etc. In both cases, no user credentials is required for token refresh  - only the `clientId`, and optionally `clientSecret` is required. If the Client App was registered without `clientSecret`, it is not required. 
 
 ### Python
 Use command token_refresh -h for help. 
-Example usage : `python3 python/generate_token.py --file ./keys_exmaple.json --tokenUrl https://auth-test.ala.org.au/cas/oidc/oidcAccessToken --clientId replaceMe --clientSecret replaceMe`
+Example usage CAS Prod: `python3 python/token_refresh.py --file ./keys_example.json --tokenUrl https://auth.ala.org.au/cas/oidc/oidcAccessToken --clientId replaceMe --clientSecret replaceMe`
+Example usage CAS Test: `python3 python/token_refresh.py --file ./keys_example.json --tokenUrl https://auth-test.ala.org.au/cas/oidc/oidcAccessToken --clientId replaceMe --clientSecret replaceMe`
+Example usage Cognito Test: `python3 python/token_refresh.py --file ./keys_example.json --tokenUrl https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token --clientId replaceMe --clientSecret replaceMe`
+Example usage Cognito Test without client secret: `python3 python/token_refresh.py --file ./keys_example.json --tokenUrl https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token --clientId replaceMe`
 
 ### Groovy
--- python/token_refresh.py
+++ python/token_refresh.py
@@ -23,9 +23,9 @@ def parse_input():
 
     # required non-positional arguments for filepath
     parser.add_argument('--file', type=str, help="Path to the JSON file containing the jwt access token, expiry and re-generation details", required=True, nargs='?')
-    parser.add_argument('--tokenUrl', type=str, help="URL for for the JWT token generation service e.g. https://auth.ala.org.au/cas/oidc/oidcAccessToken", required=True, nargs='?')
+    parser.add_argument('--tokenUrl', type=str, help="URL for for the JWT token generation service e.g. https://auth.ala.org.au/cas/oidc/oidcAccessToken, https://auth-test.ala.org.au/cas/oidc/oidcAccessToken, or https://auth-secure.auth.ap-southeast-2.amazoncognito.com/oauth2/token", required=True, nargs='?')
     parser.add_argument('--clientId', type=str, help="Client ID previously used  for JWT generation", required=True, nargs='?')
-    parser.add_argument('--clientSecret', type=str, help="Client Secret previously used for JET generation", required=True, nargs='?')
+    parser.add_argument('--clientSecret', type=str, help="Client Secret previously used for JET generation", required=False, nargs='?')
 
     args = parser.parse_args()
 
@@ -53,20 +53,29 @@ def get_token(token_config):
 
 # regenerate token, return new token and update token_obj
 def regenerate_token(token_config):
-    payload = {'refresh_token': token_obj["refresh_token"], 'grant_type': 'refresh_token', 'scope':token_obj["scope"]}
-    # refreshing token
-    r = requests.post(token_config["token_url"], data=payload, auth=(token_config["client_id"], token_config["client_secret"]))
+    # for cognito auth system use the payload below
+    payload = {'refresh_token': token_obj["refresh_token"], 'grant_type': 'refresh_token', 'scope':token_obj["scope"], 'client_id':token_config["client_id"]}
+
+    # for CAS auth system use the payload below
+    # payload = {'refresh_token': token_obj["refresh_token"], 'grant_type': 'refresh_token', 'scope':token_obj["scope"]}
+
+    # for cognito auth system use the request below
+    r = requests.post(token_config["token_url"], data=payload)
+
+    # for CAS auth system use the payload below
+    # r = requests.post(token_config["token_url"], data=payload, auth=(token_config["client_id"], token_config["client_secret"]))
+
     if r.ok:
         data = r.json()
         token_obj["access_token"] = data["access_token"]
         token_obj["id_token"] = data["id_token"]
         print("Token refreshed")
     else:
-        print("Unable to refresh access token. ", r.status_code)
+        print("Unable to refresh access token. ", r.status_code, r.content)
 
 
 def api_example_request(token_config):
-    url = "https://api.test.ala.org.au/surveys/ws/project/search?hub=ala&max=20&offset=0&isCitizenScience=false&isWorks=false&isBiologicalScience=false&isWorldWide=false&isUserPage=false&mobile=false&flimit=15"
+    url = "https://api.test.ala.org.au/occurrences/config"
     # get the JWT
     jwt = get_token(token_config)
     headers = {'user-agent': 'token-refresh/0.1.1', 'Authorization': 'Bearer {0}'.format(jwt)}
